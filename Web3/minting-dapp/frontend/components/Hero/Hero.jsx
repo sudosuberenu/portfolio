507b14f42d5caf2e2d2211b170d01f426ef1b63d
Belen Iniesta <biniesta@cano-group.com>
2022-05-26 13:22:05 +0200

    Modify style for Hero Component

--- Web3/minting-dapp/frontend/components/Hero/Hero.jsx
+++ Web3/minting-dapp/frontend/components/Hero/Hero.jsx
@@ -1,17 +1,32 @@
+import { useEffect, useCallback } from 'react';
+import { useWeb3React } from '@web3-react/core';
+
 import Button from '../Button/Button.jsx';
 
 import styles from './Hero.module.scss';
 
 export default function Hero() {
+  const {
+    library,
+    account,
+  } = useWeb3React();
+
+  useEffect(()=> {
+    
+  }, []);
+
   return (
     <section className={styles.hero}>
       <div className={styles.hero__header}>
         <ul className={styles.nav} role="tablist">
-          <li className={styles.nav__item + ' ' + styles['nav__item--active']}>Mint</li>
-          <li className={styles.nav__item}>Your NFT's</li>
+          <li className={styles.nav__item + ' ' + styles['nav__item--first']}>Mint</li>
+          <li className={styles.nav__item + ' ' + styles['nav__item--last'] + ' ' + styles['nav__item--deactive']}>Your NFT's</li>
         </ul>
       </div>
       <div className={styles.hero__body}>
+        <p>NFT Price: 0.1 Ether</p>
+        <p> Total Supply: 5</p>
+        <p> Number of NFT's Left: 3</p>
         <Button />
       </div>
       {/* {
